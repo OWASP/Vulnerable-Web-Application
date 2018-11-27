a1e9c7ee0bb5a1d373562d6159949c9b4b56d805
Ramazan Emre Erkan <remre98@gmail.com>
2018-11-27 17:02:51 +0300

    Delete hint1.php

--- FileInclusion/pages/hint1.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-	<p>
-	<div align="center"><b><h3><i> “ Even the smallest person can change the course of the future. ”</i></h3></b></div>
-	<div align="center"><b><h4> You found one!!</h4></b></div>   
-</body>
-</html>
