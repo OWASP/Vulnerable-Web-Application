2fa2bdfda1c0dbe59f35704a7a987760efe6d54a
Ramazan Emre Erkan <remre98@gmail.com>
2018-11-27 17:02:59 +0300

    Delete 2.php

--- FileInclusion/pages/2.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-	<p>
-	<div align="center"><b><h3> Did you notice anything changed? Browse the site.</h3></b></div> 
-	<!-- "There are 3 hints under the File Inclusion folder. Try to find them all in different difficulty levels. " <!-->
-</body>
-</html>
