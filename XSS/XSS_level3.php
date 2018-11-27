f01990f4bdb435bbae8946ab21c4b7951d804914
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2018-11-27 23:36:52 +0300

    Create XSS_level3.php

---
+++ XSS/XSS_level3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>REFLECTED XSS 3</title>
+</head>
+<body>
+<form method="GET" action="" name="form">
+   <p>Adınız:<input type="text" name="username"></p>
+   <input type="submit" name="Gönder">
+</form>
+<?php 
+if (isset($_GET["username"])) {
+	$user = preg_replace("/<(.*)[S,s](.*)[C,c](.*)[R,r](.*)[I,i](.*)[P,p](.*)[T,t]>/i", "", $_GET["username"]);
+	echo "$user";
+}
+ ?>
+
+
+</body>
+</html>
