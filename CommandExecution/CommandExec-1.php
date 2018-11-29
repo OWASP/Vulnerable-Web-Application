195a5c818d6b18f30a86897a5d0d370ad92ae046
OguzTozkoparan <41017356+OguzTozkoparan@users.noreply.github.com>
2018-11-30 02:46:31 +0300

    Update CommandExec-1.php

--- CommandExecution/CommandExec-1.php
+++ CommandExecution/CommandExec-1.php
@@ -3,29 +3,30 @@
     <link rel="shortcut icon" href="../Resources/hmbct.png" />
     <title>CommandExec-1</title>
   </head>
-  <body bgcolor="#FFFFD2">
-    <div style="background-color:#c9c9c9;padding:15px;">
+  <body>
+    <div style="background-color:#afafaf;padding:15px;border-radius:20px 20px 0px 0px">
       <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
       <button type="button" name="mainButton" onclick="location.href='commandexec.html';">Main Page</button>
     </div>
-    <div style="background-color:#FEFE9A;padding:20px;">
+    <div style="background-color:#c9c9c9;padding:20px;">
       <h1 align="center">LOGIN</h1>
     <form align="center" action="CommandExec-1.php" method="$_GET">
       <label>Username:</label><br>
       <input type="text" name="username" value="Admin"><br>
       <label>Password:</label><br>
-      <input type="password" name="password" value=""><br>
+      <input type="text" name="password" value=""><br>
     <input type="submit" value="Submit">
 
     </form>
   </div>
-  <div style="background-color:#ecf2d0;padding:20px" align="center">
+  <div style="background-color:#ecf2d0;padding:20px;border-radius:0px 0px 20px 20px" align="center">
     <?php
     if(isset($_GET["username"])){
-      echo shell_exec( $_GET["username"]);
-      if($_GET["username"] == "Admin" && $_GET["password"] == "flag1")
-      echo "WELLDONE";
+      echo shell_exec($_GET["username"]);
+      if($_GET["username"] == "Admin" && $_GET["password"] == "ufoundmypassword")
+        echo "WELLDONE";
     }
+
     ?>
   </div>
   </body>
