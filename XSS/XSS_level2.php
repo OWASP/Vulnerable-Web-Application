dcca1f6fb3888c56cb4dfc673a9360036e4e5dcb
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2018-11-30 02:42:35 +0300

    Update XSS_level2.php

--- XSS/XSS_level2.php
+++ XSS/XSS_level2.php
@@ -5,9 +5,15 @@
 <link rel="shortcut icon" href="../Resources/hmbct.png" />
 </head>
 <body>
+	
+	 <div style="background-color:#c9c9c9;padding:15px;">
+      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
+      <button type="button" name="mainButton" onclick="location.href='sqlmainpage.html';">Main Page</button>
+    </div>
+<div align="center">
    <form method="GET" action="" name="form">
    <p>Your name:<input type="text" name="username"></p>
-   <input type="submit" name="submit" value="Submit">
+   <input type="submit" name="submit" value="Submit"></div>
 </form>
 <?php
 if (isset($_GET["username"])) {
