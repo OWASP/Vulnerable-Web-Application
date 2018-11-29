cdc2d2a969649be30d317e1b38e5f5be87e74885
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2018-11-30 01:06:56 +0300

    Update XSS_level2.php

--- XSS/XSS_level2.php
+++ XSS/XSS_level2.php
@@ -6,13 +6,13 @@
 </head>
 <body>
    <form method="GET" action="" name="form">
-   <p>Adınız:<input type="text" name="username"></p>
+   <p>Your name:<input type="text" name="username"></p>
    <input type="submit" name="Gönder">
 </form>
 <?php
 if (isset($_GET["username"])) {
  	$user = str_replace("<script>", "",$_GET["username"]);
-	echo "$user";
+	echo "Your name is "."$user";
 }
 ?>
 
