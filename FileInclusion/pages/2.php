8515f18f267ba08a2865b3bc8dd61498cec27a5f
Ramazan Emre Erkan <remre98@gmail.com>
2018-11-30 00:18:35 +0300

    Update 2.php

--- FileInclusion/pages/2.php
+++ FileInclusion/pages/2.php
@@ -3,7 +3,7 @@
 	<p>
 	<div align="center"><b><h1> Did you notice anything changed? Browse the site.</h1></b></div> 
 	<!-- "There are 2 hints under the File Inclusion folder. They should be there ->  " <!-->
-	<!-- "Hint1 : C:\xampp\htdocs\FileInclusion\pages\dontOpen\hint1 <!-->
-	<!-- "Hint2 : C:\xampp\htdocs\FileInclusion\hint2\hint2.php" <!-->
+	<!-- "Hint1 : FileInclusion\pages\dontOpen\hint1 <!-->
+	<!-- "Hint2 : FileInclusion\hint2\hint2.php" <!-->
 </body>
 </html>
