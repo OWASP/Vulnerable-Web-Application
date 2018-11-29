7cad7f76e4c72f7d869badf1120f38d403aa0019
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2018-11-30 02:45:00 +0300

    Update XSS_level4.php

--- XSS/XSS_level4.php
+++ XSS/XSS_level4.php
@@ -4,10 +4,17 @@
 	<title>XSS 4</title>
 </head>
 <body>
+	
+	 <div style="background-color:#c9c9c9;padding:15px;">
+      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
+      <button type="button" name="mainButton" onclick="location.href='sqlmainpage.html';">Main Page</button>
+    </div>
+<div align="center">
 <form method="GET" action="" name="form">
    <p>Your name:<input type="text" name="username"></p>
    <input type="submit" name="submit" value="Submit">
 </form>
+	</div>
 <?php 
 if (isset($_GET["username"])) {
     $values = array("script", "prompt", "alert", "h1");
