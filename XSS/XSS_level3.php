1310ec3301dd22436bd35c61cc92ad3ba0397930
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2018-11-30 01:07:04 +0300

    Update XSS_level3.php

--- XSS/XSS_level3.php
+++ XSS/XSS_level3.php
@@ -6,13 +6,13 @@
 </head>
 <body>
 <form method="GET" action="" name="form">
-   <p>Adınız:<input type="text" name="username"></p>
+   <p>Your name:<input type="text" name="username"></p>
    <input type="submit" name="Gönder">
 </form>
 <?php 
 if (isset($_GET["username"])) {
 	$user = preg_replace("/<(.*)[S,s](.*)[C,c](.*)[R,r](.*)[I,i](.*)[P,p](.*)[T,t]>/i", "", $_GET["username"]);
-	echo "$user";
+	echo "Your name is "."$user";
 }
  ?>
 
