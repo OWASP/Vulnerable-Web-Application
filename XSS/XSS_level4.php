4b8ee90637b37957dec6d17347f17150bff08d04
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2018-11-29 22:53:51 +0300

    Create XSS_level4.php

---
+++ XSS/XSS_level4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>XSS 4</title>
+</head>
+<body>
+<form method="GET" action="" name="form">
+   <p>Your name:<input type="text" name="username"></p>
+   <input type="submit" name="Gönder">
+</form>
+<?php 
+if (isset($_GET["username"])) {
+    $values = array("script", "prompt", "alert", "h1");
+
+ 	$user = str_replace($values, " ",$_GET["username"]);
+	echo "$user";
+}
+
+ ?>
+
+</body>
+</html>
