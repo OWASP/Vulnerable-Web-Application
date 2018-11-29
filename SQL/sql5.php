fe2d3d7c0a7bbcfc4b3ae7273ba1bb5639fe1c9c
Fatih Çelik <fcelik.ft@gmail.com>
2018-11-30 05:47:15 +0600

    Update sql5.php

--- SQL/sql5.php
+++ SQL/sql5.php
@@ -5,16 +5,22 @@
 	<link rel="shortcut icon" href="../Resources/hmbct.png" />
 </head>
 <body>
+	<div style="background-color:#c9c9c9;padding:15px;">
+      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
+      <button type="button" name="mainButton" onclick="location.href='sqlmainpage.html';">Main Page</button>
+	</div>
 
+	<div align="center">
 	<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" >
 		<p>Give me book's number and I give you book's name in my library.</p>
 		Book's number : <input type="text" name="number">
-		<input type="submit" name="submit" value="Submit">
+		<input type="submit" name="submit">
 		<!--<p>You hacked me again?
 			   But I updated my code
 			</p>
 		-->
 	</form>
+	</div>
 
 <?php
 	$servername = "localhost";
