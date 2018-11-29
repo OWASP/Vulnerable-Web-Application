e503fc88f8ac69ef811ad97ae95bbd2b904de6dd
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2018-11-30 02:45:56 +0300

    Update XSS_level5.php

--- XSS/XSS_level5.php
+++ XSS/XSS_level5.php
@@ -5,10 +5,17 @@
 <link rel="shortcut icon" href="../Resources/hmbct.png" />
 </head>
 <body>
+    
+	 <div style="background-color:#c9c9c9;padding:15px;">
+      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
+      <button type="button" name="mainButton" onclick="location.href='xssmainpage.html';">Main Page</button>
+    </div>
+<div align="center">
 <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="form">
    <p>Your name:<input type="text" name="username"></p>
    <input type="submit" name="submit" value="Submit">
 </form>
+    </div>
 
 <?php 
 if (isset($_GET["username"])) {
