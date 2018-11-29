b5737c18aa20b89bb909f53520c4099f9a692812
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2018-11-30 02:32:31 +0300

    Update XSS_level1.php

--- XSS/XSS_level1.php
+++ XSS/XSS_level1.php
@@ -7,7 +7,7 @@
 <body>
    <form method="GET" action="" name="form">
    <p>Your name:<input type="text" name="username"></p>
-   <input type="submit" name="Gönder">
+   <input type="submit" name="Gönder" value="Submit">
 </form>
 <?php
 if(isset($_GET["username"]))
