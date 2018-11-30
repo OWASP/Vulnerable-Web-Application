13c45b3707bdf2677c95d6482eb4007411687add
Fatih Çelik <fcelik.ft@gmail.com>
2018-11-30 16:56:17 +0600

    Update fileupload3.php

--- FileUpload/fileupload3.php
+++ FileUpload/fileupload3.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<link rel="shortcut icon" href="../Resources/hmbct.png" />
+</head>
 <body>
-
-	 <div style="background-color:#c9c9c9;padding:15px;">
+<div style="background-color:#c9c9c9;padding:15px;">
       <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
       <button type="button" name="mainButton" onclick="location.href='fileupl.html';">Main Page</button>
-    </div>
+</div>
 <div align="center">
 <form action="" method="post" enctype="multipart/form-data">
-    Select image -> 
-    <input type="file" name="file" id="file">
-    <hr>
+    <br>
+    <b>Select image : </b> 
+    <input type="file" name="file" id="file" style="border: solid;">
     <input type="submit" value="Submit" name="submit">
 </form>
 	</div>
