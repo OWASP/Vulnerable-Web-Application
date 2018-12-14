36f63009d4ed6b802c12dcfabd4294116be63072
Ramazan Emre Erkan <remre98@gmail.com>
2018-12-14 21:23:56 +0300

    Update 2.php

--- FileInclusion/pages/2.php
+++ FileInclusion/pages/2.php
@@ -3,7 +3,8 @@
 	<p>
 	<div align="center"><b><h1> Did you notice anything changed? Browse the site.</h1></b></div> 
 	<!-- "There are 2 hints under the File Inclusion folder. They should be there ->  " <!-->
-	<!-- "Hint1 : FileInclusion\pages\dontOpen\hint1 <!-->
+	<!-- "Hint1 : FileInclusion\pages\dontOpen\hint1.php <!-->
 	<!-- "Hint2 : FileInclusion\hint2\hint2.php" <!-->
+	<!-- "Paste the paths to the URL is not a solution!!" <!-->
 </body>
 </html>
