8c5b5039bb37c4ea7c70d4d254b1d159168f1259
iamdhil <dhileepan19@mail.com>
2022-02-22 12:20:11 -0500

    added change

--- SQL/sql2.php
+++ SQL/sql2.php
@@ -28,6 +28,7 @@
 	// Create connection
 	$conn = new mysqli($servername, $username, $password,$db);
 
+
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
